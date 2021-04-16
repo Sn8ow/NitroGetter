0e4ee95b572227c74b6f53f28273a94fd22b1104
sn8ow <80784394+Sn8ow@users.noreply.github.com>
2021-04-16 10:52:18 +0200

    Update NitroGetter.plugin.js

--- NitroGetter.plugin.js
+++ NitroGetter.plugin.js
@@ -38,7 +38,7 @@ module.exports = (() => {
                 "discord_id": "407348579376693260",
                 "github_username": "respecting"
             }],
-            "version": "1.9.4",
+            "version": "1.9.5",
             "description": "Unlock all screensharing modes, and use cross-server emotes & gif emotes, Discord wide! (You CANNOT upload 100MB files though. :/)",
             "github": "https://github.com/Sn8ow/NitroGetter",
             "github_raw": "https://raw.githubusercontent.com/Sn8ow/NitroGetter/main/NitroGetter.plugin.js"
